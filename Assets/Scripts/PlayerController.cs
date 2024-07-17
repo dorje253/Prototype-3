4a2cdccd6c3ff722b0ec8f291df1f3318639eb53
dorje253 <tdorje070@gmail.com>
2024-07-17 17:16:38 +0545

    add Challenge , particle and sound in game
    add the challenege assets and solving bug
    
    add the particle and sound in game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,8 +9,12 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
-
     private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dartParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +22,7 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         
     }
 
@@ -28,6 +33,8 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up *jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dartParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
@@ -35,12 +42,17 @@ public class PlayerController : MonoBehaviour
         
         if(collision.gameObject.CompareTag("Ground")){
             isOnGround = true;
+            dartParticle.Play();
         }else if (collision.gameObject.CompareTag("Obstacle")){
             gameOver = true;
             Debug.Log("Game Over");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
-
+            explosionParticle.Play();
+            dartParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
+
+    
 }
